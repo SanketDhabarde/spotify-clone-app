0ef649efe09bc8d19b074cbb933bf2f3c1c5f95f
SanketDhabarde <sanketdhabarde13@gmail.com>
2021-05-17 16:44:39 +0530

    feat: login with spotify and get the user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Login from './Components/Login/Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+// creating the new instance of spotify web api
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const _token = getTokenFromUrl;
+    // empty the url after getting token
+    window.location.hash = ""; 
+
+    if(_token){
+      setToken(_token);
+      // connect spotify with react using token
+      spotify.setAccessToken(_token);
+      // get the user from spotify
+      spotify.getMe().then(user => {
+        console.log(user);
+      })
+    }
+  }, []);
+
+
   return (
     <div className="app">
-      <h1>hello guys!</h1>
+      {
+        token ? (
+          <h1>im logged in</h1>
+        ) : (
+          <Login/>
+        )
+      }
     </div>
   );
 }
---
+++ src/spotify.js
@@ -0,0 +1,23 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndPoint = "https://accounts.spotify.com/authorize";
+
+const clientId = "576e54ea425148cc9cbbf8ba200962ec";
+
+const redirectUri = "http://localhost:3000/";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = new URLSearchParams(window.location.hash.substring(1)).get("access_token");
+
+export const loginUrl = `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+  )}&response_type=token&show_dialog=true`;
+
+// %20 is ascii code for space
