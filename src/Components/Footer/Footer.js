7ce57990bf0db6bc7d8e48a617649c03a293e94d
SanketDhabarde <sanketdhabarde13@gmail.com>
2021-05-18 17:25:58 +0530

    style

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import './Footer.css';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import PlaylistPlayIcon from '@material-ui/icons/PlaylistPlay';
+import { Grid, Slider } from '@material-ui/core';
 
 function Footer() {
     return (
         <div className="footer">
-            <h1>i am a footer</h1>
+            <div className="footer__left">
+                <img className="footer__albumLogo"
+                src="https://images.unsplash.com/photo-1598386860329-3d7214504a3f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c29uZyUyMGFsYnVtfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" />
+                <div className="footer__songInfo">
+                    <h4>No songs playing</h4>
+                    <p>...</p>
+                </div>
+            </div>
+            <div className="footer__center">
+                <ShuffleIcon/>
+                <SkipPreviousIcon/>
+                <PlayCircleFilledIcon fontSize="large"/>
+                <SkipNextIcon/>
+                <RepeatIcon/>
+            </div>
+            <div className="footer__right">
+                <Grid container spacing={2}>
+                    <Grid item>
+                        <PlaylistPlayIcon />
+                    </Grid>
+                    <Grid item>
+                        <VolumeDownIcon/>
+                    </Grid>
+                    <Grid item xs>
+                        <Slider/>
+                    </Grid>
+                </Grid>
+            </div>
         </div>
     )
 }
